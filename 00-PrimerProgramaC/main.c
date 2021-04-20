f7dccd0437cbffacf38984a2dd3c7b073c4123bf
CaminoFernando <camino.fernando@gmail.com>
2021-04-19 23:03:33 -0300

    ok

--- 00-PrimerProgramaC/main.c
+++ 00-PrimerProgramaC/main.c
@@ -1,8 +1,11 @@
+#include <conio.h>
 #include <stdio.h>
 
 int main()
 {
-    printf( "Hola mundo\n" );
+    printf( "Hola profes!\n" );
+
+    getch(); /* Para que el programa no se cierre solo */
 
     return 0;
 }
